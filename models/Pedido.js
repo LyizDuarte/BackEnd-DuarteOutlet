305bba0ea6b3e3d125db4f30b23d6db75cbd938b
LyizDuarte <looduarte21@gmail.com>
2025-04-24 12:26:23 -0300

    fix: valor total somando corretamente

--- models/Pedido.js
+++ models/Pedido.js
@@ -1,12 +1,12 @@
-import PedidoDAO from "../DAO/PedidoDAO.js";
+import PedidoDAO from "../DAO/PedidoDAO.js"
 
 export default class Pedido {
-  #codigo;
-  #nome;
-  #telefone;
-  #endereco;
-  #valorTotal;
-  #itensPedido = [];
+  #codigo
+  #nome
+  #telefone
+  #endereco
+  #valorTotal
+  #itensPedido = []
   constructor(
     codigo = 0,
     nome,
@@ -15,63 +15,63 @@ export default class Pedido {
     valorTotal,
     itensPedido = []
   ) {
-    this.#codigo = codigo;
-    this.#nome = nome;
-    this.#telefone = telefone;
-    this.#endereco = endereco;
-    this.#valorTotal = valorTotal;
-    this.#itensPedido = itensPedido;
+    this.#codigo = codigo
+    this.#nome = nome
+    this.#telefone = telefone
+    this.#endereco = endereco
+    this.#valorTotal = valorTotal
+    this.#itensPedido = itensPedido
   }
 
   get codigo() {
-    return this.#codigo;
+    return this.#codigo
   }
   set codigo(codigo) {
-    this.#codigo = codigo;
+    this.#codigo = codigo
   }
 
   get nome() {
-    return this.#nome;
+    return this.#nome
   }
   set nome(nome) {
-    this.#nome = nome;
+    this.#nome = nome
   }
 
   get telefone() {
-    return this.#telefone;
+    return this.#telefone
   }
   set telefone(telefone) {
-    this.#telefone = telefone;
+    this.#telefone = telefone
   }
 
   get endereco() {
-    return this.#endereco;
+    return this.#endereco
   }
 
   set endereco(endereco) {
-    this.#endereco = endereco;
+    this.#endereco = endereco
   }
   get valorTotal() {
-    let valorTotal = 0;
+    let valorTotal = 0
     for (const item of this.#itensPedido) {
-      valorTotal += Number(item.servico.valorServico);
+      valorTotal += Number(item.quantidade) * Number(item.servico.valorServico)
     }
-    return valorTotal;
+    return valorTotal
   }
   set valorTotal(valorTotal) {
-    this.#valorTotal = valorTotal;
+    this.#valorTotal = valorTotal
   }
 
   get itensPedido() {
-    return this.#itensPedido;
+    return this.#itensPedido
   }
 
   set itensPedido(itensPedido) {
-    this.#itensPedido = itensPedido;
+    this.#itensPedido = itensPedido
   }
 
   async gravar() {
-    const pedDao = new PedidoDAO();
-    await pedDao.gravar(this);
+    const pedDao = new PedidoDAO()
+    await pedDao.gravar(this)
   }
 }
